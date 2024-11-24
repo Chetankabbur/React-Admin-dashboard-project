71e6f212f3c21e0c51aecfd950250e79d6f0f494
Chetankabbur <122073535+Chetankabbur@users.noreply.github.com>
2024-11-24 13:59:50 +0530

    Add files via upload

---
+++ build/precache-manifest.00db3cbcb65374d09e960c8dcbee8244.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eddefbc2a51a26dab2f96990be993895",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f8b2337b62b6363cf97",
+    "url": "/static/css/2.85f5ce81.chunk.css"
+  },
+  {
+    "revision": "b28ee2c2c62e2977296e",
+    "url": "/static/css/main.93a0e8b6.chunk.css"
+  },
+  {
+    "revision": "3f8b2337b62b6363cf97",
+    "url": "/static/js/2.aa2a0f75.chunk.js"
+  },
+  {
+    "revision": "b28ee2c2c62e2977296e",
+    "url": "/static/js/main.084cda00.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
